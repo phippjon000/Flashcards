6f63160e1d0c83c0c60a9a2c07277f0d754b648c
Jonathan Phippen <jphippen@cs.washington.edu>
2016-10-17 19:55:36 -0700

    initial commit

---
+++ CardManager.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class CardManager {
+   private Queue<Card> cards;
+   private int cardsSize;
+   private int cardsLeft;
+   private boolean valid;
+   
+   public CardManager(File cardValues) {
+      cards = new LinkedList<Card>();
+      cardsSize = 0;
+	  cardsLeft = 0;
+	  valid = true;
+	  
+      try (BufferedReader br = new BufferedReader(new FileReader(cardValues))) {
+		  String cardString;
+	     while ((cardString = br.readLine()) != null) {
+		    String[] cardParts = cardString.split(",");
+			Card card = new Card(cardParts[0], cardParts[1]);
+			cards.add(card);
+			cardsSize++;
+		 }
+		 
+		 cardsLeft = cardsSize;
+		 shuffleCards();
+	  } catch (Exception e) {
+	     System.err.println("An error occured when parsing file " + cardValues.getName() + ", " + e.getMessage());
+		 valid = false;
+	  }
+   }
+   
+   public Card getNextCard() {
+      Card nextCard = cards.remove();
+	  cards.add(nextCard);
+	  cardsLeft--;
+	  if (cardsLeft <= 0) {
+	     shuffleCards();
+		 cardsLeft = cardsSize;
+	  }
+	  
+	  return nextCard;
+   }
+   
+   private void shuffleCards() {
+      List<Card> oldCards =  new ArrayList<Card>();
+	  for (int i = 0; i < cardsSize; i++) {
+	     oldCards.add(cards.remove());
+	  }
+	  Collections.shuffle(oldCards);
+	  for (int i = 0; i < oldCards.size(); i++) {
+		 cards.add(oldCards.get(i));
+	  }
+   }
+   
+   public boolean isValid() {
+	  return valid;
+   }
+}
---
+++ Flashcards.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Flashcards {
+   public static void main(String[] args) {
+      String filePath = args[0];
+	  CardManager cm = new CardManager(new File(filePath));
+	  Scanner sc = new Scanner(System.in);
+	  
+	  if (!cm.isValid()) {
+		 System.err.println("An error occured while initializing Flashcards");
+		 System.exit(1);
+	  }
+	  
+	  System.out.println("Welcome to Flashcards!");
+	  while(true) {
+	     Card card = cm.getNextCard();
+		 System.out.println("Question: " + card.getQuestion());
+		 String userAnswer = sc.nextLine();
+		 if (card.getAnswer().toLowerCase().contains(userAnswer)) {
+		    System.out.println("Correct! Answer is: " + card.getAnswer());
+		 } else {
+		    System.out.println("Incorrect. Answer is: " + card.getAnswer());
+		 }
+	  }
+   }
+}
